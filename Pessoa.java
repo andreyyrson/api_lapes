52e2e6aa85d3550d1d0267a199184ea46c5d194f
Andrey de matos Goncalves <andreyrson2000@gmail.com>
2024-08-29 10:46:03 -0300

    Add files via upload
    //aqui é o teste da API para testar a atribuiçao de dados para os metodos acima aonde foi colocado informaçoes por meio de um arquivo json

---
+++ Pessoa.java
@@ -0,0 +1,22 @@
+package apilapes.com.br.apilapes.modelo;
+
+public class Pessoa {
+    //atributos
+    private String nome;
+    private int idade;
+
+    //get e set
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+   
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
